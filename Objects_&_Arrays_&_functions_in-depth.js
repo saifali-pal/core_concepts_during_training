4a488f6fbd8ddde2ca34e6464b278b87e8e809eb
saifali-pal <saif.ali@mergestack.com>
2025-10-15 13:17:26 +0500

    weekend-practice

---
+++ Objects_&_Arrays_&_functions_in-depth.js
@@ -0,0 +1,8 @@
+//https://medium.com/@aamchora/javascript-objects-in-depth-6faba91eab50
+
+//https://noncodersuccess.medium.com/diving-deep-into-javascript-arrays-advanced-techniques-and-real-world-examples-fa029da35f72
+
+//https://medium.com/@mohdtalib.dev/deep-dive-into-javascript-functions-a-practical-guide-7728adea4d0f
+
+//conditional & Loops
+//https://dev.to/codenutt/everything-you-should-know-about-javascript-variables-p4f
--- event_loop.js
+++ event_loop.js
@@ -1,1 +1,9 @@
-//for making responsive web applications  
+//for making responsive web applications
+//different types of tasks in JavaScript
+
+//simple example:
+//imagine you are running a todo-list Event loop is like a manager that ensures
+//tasks are completed in the right order depending on their type and urgecny
+
+//https://medium.com/@ignatovich.dm/the-javascript-event-loop-explained-with-examples-d8f7ddf0861d
+
